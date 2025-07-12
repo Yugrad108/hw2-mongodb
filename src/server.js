fd8ca658197ec735b6ae42083e2e3063d7b8a313
Yurii <Jahnavart@gmail.com>
2025-07-12 19:11:43 +0300

    done swagger 1

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', 3000));
 
@@ -30,6 +31,8 @@ export function setupServer() {
   );
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+
+  app.use('/api-docs', swaggerDocs());
 
   app.get('/', (req, res) => {
     res.status(200).json({
